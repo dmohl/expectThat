3508ee3763700348577291e7d91abbe23faa7437
Dan Mohl <dmohl@yahoo.com>
2012-01-16 15:54:00 -0600

    Completed the ExpectThat.Mocha NuGet package.

--- NuGet/ExpectThat.Mocha/Content/Scripts/Specs/index.html.pp
+++ NuGet/ExpectThat.Mocha/Content/Scripts/Specs/index.html.pp
@@ -5,9 +5,9 @@
   <link rel="stylesheet" href="https://raw.github.com/visionmedia/mocha/master/mocha.css" />
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script src="https://raw.github.com/visionmedia/mocha/master/mocha.js"></script>
-  <script type="text/javascript" src="../../lib/expectThat.mocha.js"></script>
+  <script type="text/javascript" src="expectThat.mocha.js"></script>
   <script>mocha.setup('bdd')</script>
-  <script src="./min/example.spec.js"></script>
+  <script src="example.spec.js"></script>
   <script>
     $(function () {
       mocha
